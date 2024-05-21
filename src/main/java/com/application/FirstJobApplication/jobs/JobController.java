778010b136a91e36fa433af15acaa75dd544bfaa
ugoala_hope <ugoala.hope11@gmail.com>
2024-05-21 14:49:06 +0200

    Initial commit

---
+++ src/main/java/com/application/FirstJobApplication/jobs/JobController.java
@@ -0,0 +1,36 @@
+package com.application.FirstJobApplication.jobs;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class JobController {
+    private JobService jobService;
+
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping("/jobs/{id}")
+    public Job getJobById(@PathVariable Long id){
+        Job job = jobService.getJobById(id);
+        return job;
+
+    }
+
+    @GetMapping("/jobs")
+    public List<Job> findAll() {
+        return jobService.findAll();
+
+    }
+
+    @PostMapping("/jobs")
+    public String CreateNewJobs(@RequestBody Job job){
+        jobService.createJob(job);
+        return "The job was added successfully";
+    }
+
+}
---
+++ src/main/java/com/application/FirstJobApplication/jobs/JobService.java
@@ -0,0 +1,12 @@
+package com.application.FirstJobApplication.jobs;
+
+import java.util.List;
+
+public interface JobService {
+
+
+    List<Job> findAll();
+    void createJob(Job job);
+
+    Job getJobById(Long id);
+}
