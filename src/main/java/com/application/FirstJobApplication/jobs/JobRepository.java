4492d11b10bb8bf33d7e09817350825eb5b73ea9
ugoala_hope <ugoala.hope11@gmail.com>
2025-01-31 15:33:59 +0100

    Adding Repository

--- src/main/java/com/application/FirstJobApplication/jobs/JobRepository.java
+++ src/main/java/com/application/FirstJobApplication/jobs/JobRepository.java
@@ -1,4 +1,6 @@
 package com.application.FirstJobApplication.jobs;
 
-public interface JobRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job, Long> {
 }
